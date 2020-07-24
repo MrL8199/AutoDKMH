e23014d5c6e6ae63f726106b920bca210355fcb3
Đinh Văn Lực <dinhvanluc69@gmail.com>
2020-07-24 10:52:05 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace AutoDKMH
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.tbCookiesValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbListSubjects = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.timerAntiTimeoutSession = new System.Windows.Forms.Timer(this.components);
+            this.tbCookiesKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCSE_ID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbCRD_ID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbLichhoc = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbhidListReg = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbhidSubNum = new System.Windows.Forms.TextBox();
+            this.cbByMajor = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblStatus.Location = new System.Drawing.Point(285, 310);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(77, 17);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Đang chờ..";
+            // 
+            // tbCookiesValue
+            // 
+            this.tbCookiesValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbCookiesValue.Location = new System.Drawing.Point(359, 69);
+            this.tbCookiesValue.Name = "tbCookiesValue";
+            this.tbCookiesValue.Size = new System.Drawing.Size(131, 23);
+            this.tbCookiesValue.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(29, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Cookies:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Đăng ký";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(29, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "List Subjects:";
+            // 
+            // tbListSubjects
+            // 
+            this.tbListSubjects.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbListSubjects.Location = new System.Drawing.Point(191, 101);
+            this.tbListSubjects.Name = "tbListSubjects";
+            this.tbListSubjects.Size = new System.Drawing.Size(299, 23);
+            this.tbListSubjects.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(29, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Account";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbPassword.Location = new System.Drawing.Point(359, 40);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(131, 23);
+            this.tbPassword.TabIndex = 1;
+            // 
+            // tbID
+            // 
+            this.tbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbID.Location = new System.Drawing.Point(191, 40);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(162, 23);
+            this.tbID.TabIndex = 0;
+            // 
+            // timerAntiTimeoutSession
+            // 
+            this.timerAntiTimeoutSession.Enabled = true;
+            this.timerAntiTimeoutSession.Interval = 50000;
+            this.timerAntiTimeoutSession.Tick += new System.EventHandler(this.timerAntiTimeoutSession_Tick);
+            // 
+            // tbCookiesKey
+            // 
+            this.tbCookiesKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbCookiesKey.Location = new System.Drawing.Point(191, 69);
+            this.tbCookiesKey.Name = "tbCookiesKey";
+            this.tbCookiesKey.Size = new System.Drawing.Size(162, 23);
+            this.tbCookiesKey.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(29, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List CSE_ID:";
+            // 
+            // tbCSE_ID
+            // 
+            this.tbCSE_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbCSE_ID.Location = new System.Drawing.Point(191, 130);
+            this.tbCSE_ID.Name = "tbCSE_ID";
+            this.tbCSE_ID.Size = new System.Drawing.Size(299, 23);
+            this.tbCSE_ID.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(29, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "List CRD_ID:";
+            // 
+            // tbCRD_ID
+            // 
+            this.tbCRD_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbCRD_ID.Location = new System.Drawing.Point(191, 159);
+            this.tbCRD_ID.Name = "tbCRD_ID";
+            this.tbCRD_ID.Size = new System.Drawing.Size(299, 23);
+            this.tbCRD_ID.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label6.Location = new System.Drawing.Point(29, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "List Lichhoc:";
+            // 
+            // tbLichhoc
+            // 
+            this.tbLichhoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbLichhoc.Location = new System.Drawing.Point(191, 188);
+            this.tbLichhoc.Name = "tbLichhoc";
+            this.tbLichhoc.Size = new System.Drawing.Size(299, 23);
+            this.tbLichhoc.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label7.Location = new System.Drawing.Point(29, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "hidListReg:";
+            // 
+            // tbhidListReg
+            // 
+            this.tbhidListReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbhidListReg.Location = new System.Drawing.Point(191, 217);
+            this.tbhidListReg.Name = "tbhidListReg";
+            this.tbhidListReg.Size = new System.Drawing.Size(299, 23);
+            this.tbhidListReg.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label8.Location = new System.Drawing.Point(29, 246);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 17);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "hidSubNum:";
+            // 
+            // tbhidSubNum
+            // 
+            this.tbhidSubNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbhidSubNum.Location = new System.Drawing.Point(191, 246);
+            this.tbhidSubNum.Name = "tbhidSubNum";
+            this.tbhidSubNum.Size = new System.Drawing.Size(299, 23);
+            this.tbhidSubNum.TabIndex = 2;
+            // 
+            // cbByMajor
+            // 
+            this.cbByMajor.AutoSize = true;
+            this.cbByMajor.Location = new System.Drawing.Point(28, 278);
+            this.cbByMajor.Name = "cbByMajor";
+            this.cbByMajor.Size = new System.Drawing.Size(64, 17);
+            this.cbByMajor.TabIndex = 4;
+            this.cbByMajor.Text = "ByMajor";
+            this.cbByMajor.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 347);
+            this.Controls.Add(this.cbByMajor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbhidSubNum);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbhidListReg);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbLichhoc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbCRD_ID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbCSE_ID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbListSubjects);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbCookiesKey);
+            this.Controls.Add(this.tbCookiesValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox tbCookiesValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbListSubjects;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Timer timerAntiTimeoutSession;
+        private System.Windows.Forms.TextBox tbCookiesKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCSE_ID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbCRD_ID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbLichhoc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbhidListReg;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbhidSubNum;
+        private System.Windows.Forms.CheckBox cbByMajor;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,196 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace AutoDKMH
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Register(string requestBody)
+        {
+            lblStatus.Text = "Đăng ký..";
+            var client = new RestClient("http://dangkyhoc.mta.edu.vn/Register/RegisterUnder.asp?command=update");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Host", "dangkyhoc.mta.edu.vn");
+            request.AddHeader("Connection", "keep-alive");
+            request.AddHeader("Cache-Control", "max-age=0");
+            request.AddHeader("Origin", " http://dangkyhoc.mta.edu.vn");
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            request.AddHeader("DNT", "1");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36";
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.AddHeader("Referer", "http://dangkyhoc.mta.edu.vn/Register/RegisterUnder.asp");
+            request.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Accept-Language", "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5");
+            request.AddHeader("Cookie", tbCookiesKey.Text + "=" + tbCookiesValue.Text);
+            request.AddCookie(tbCookiesKey.Text, tbCookiesValue.Text);
+            request.AddParameter("application/x-www-form-urlencoded", requestBody, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            lblStatus.Text = "Xong..";
+        }
+
+        private void Login()
+        {
+            lblStatus.Text = "Đăng nhập..";
+            try
+            {
+                var client = new RestClient("http://dangkyhoc.mta.edu.vn/dkmh/login.asp");
+                client.Timeout = -1;
+                client.FollowRedirects = false;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Host", "dangkyhoc.mta.edu.vn");
+                request.AddHeader("Cache-Control", " max-age=0");
+                request.AddHeader("Origin", " http://dangkyhoc.mta.edu.vn");
+                request.AddHeader("Upgrade-Insecure-Requests", " 1");
+                request.AddHeader("DNT", " 1");
+                request.AddHeader("Content-Type", " application/x-www-form-urlencoded");
+                client.UserAgent = " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36";
+                request.AddHeader("Accept", " text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+                request.AddHeader("Referer", " http://dangkyhoc.mta.edu.vn/dkmh/login.asp");
+                request.AddHeader("Accept-Encoding", " gzip, deflate");
+                request.AddHeader("Accept-Language", " vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5");
+                request.AddHeader("Cookie", tbCookiesKey.Text + "=" + tbCookiesValue.Text);
+                request.AddParameter(" application/x-www-form-urlencoded", "chkSubmit=ok&txtLoginId=" + tbID.Text + "&txtPassword=" + tbPassword.Text + "&txtSel=1", ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                //tbCookiesKey.Text = response.Cookies.SingleOrDefault(x => x.Name.Contains("ASP")).Name.ToString();
+                //tbCookiesValue.Text = response.Cookies.SingleOrDefault(x => x.Name.Contains("ASP")).Value.ToString();
+                File.WriteAllText("account.txt", tbID.Text + "|" + tbPassword.Text);
+                File.WriteAllText("cookies.txt", tbCookiesKey.Text+"|"+tbCookiesValue.Text);
+                lblStatus.Text = "Đăng nhập thành công";
+            }
+            catch (Exception)
+            {
+                lblStatus.Text = "Lỗi đăng nhập";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login();
+            GetListSubjects();
+            StringBuilder reqBody = new StringBuilder();
+            BuildRequestBody(reqBody);
+            for (int i = 0; i < 10; i++)
+            {
+                Register(reqBody.ToString());
+            }
+        }
+
+        private void appendResquestBody(StringBuilder resBody, string param, List<string> listParam)
+        {
+            resBody.Append(@"&"+param+"=");
+            foreach (string parameterID in listParam)
+            {
+                resBody.Append(@",");
+                resBody.Append(parameterID);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("cookies.txt"))
+            {
+                File.Create("cookies.txt");
+            }
+            tbCookiesKey.Text = File.ReadAllText("cookies.txt").Split('|')[0];
+            tbCookiesValue.Text = File.ReadAllText("cookies.txt").Split('|')[1];
+            if (!File.Exists("account.txt"))
+            {
+                File.Create("account.txt");
+            }
+            tbID.Text = File.ReadAllText("account.txt").Split('|')[0];
+            tbPassword.Text = File.ReadAllText("account.txt").Split('|')[1];
+            tbListSubjects.Text = File.ReadAllText("subjects.txt");
+        }
+
+        private void timerAntiTimeoutSession_Tick(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void GetListSubjects()
+        {
+            lblStatus.Text = "Lấy danh sách môn học..";
+            int subNum = 0;
+            var client = new RestClient("http://dangkyhoc.mta.edu.vn/Register/RegisterTab.asp?selViewType=All");
+            if (cbByMajor.Checked)
+            {
+                client = new RestClient("http://dangkyhoc.mta.edu.vn/Register/RegisterTab.asp?selViewType=ByMajor");
+            }
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddCookie(tbCookiesKey.Text, tbCookiesValue.Text);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(response.Content);
+            var htmlNodes = document.DocumentNode.SelectNodes("//*[@name=\"hidCseCode\"]");
+
+            int i = 0;
+            foreach(var node in htmlNodes)
+            {
+                if (tbListSubjects.Text.Contains(node.Attributes["value"].Value))
+                {
+                    string CSE_ID = document.DocumentNode.SelectNodes("//*[@name=\"hidCSE_ID\"]").ElementAt(htmlNodes.GetNodeIndex(node)).Attributes["value"].Value;
+                    string hidCRD_ID = document.DocumentNode.SelectNodes("//*[@name=\"hidCRD_ID\"]").ElementAt(htmlNodes.GetNodeIndex(node)).Attributes["value"].Value;
+                    string hidLichhoc = document.DocumentNode.SelectNodes("//*[@name=\"hidLichhoc\"]").ElementAt(htmlNodes.GetNodeIndex(node)).Attributes["value"].Value;
+                    string hidNumCrd = document.DocumentNode.SelectNodes("//*[@name=\"hidNumCrd\"]").ElementAt(htmlNodes.GetNodeIndex(node)).Attributes["value"].Value;
+
+                    tbCSE_ID.Text += "," + CSE_ID;
+                    tbhidListReg.Text += CSE_ID + ",";
+                    tbCRD_ID.Text += "," + hidCRD_ID;
+                    tbLichhoc.Text += "," + hidLichhoc;
+                    subNum += Convert.ToInt32(hidNumCrd);
+                    tbhidSubNum.Text = subNum.ToString();
+                }
+                //Console.WriteLine(node.Attributes["value"].Value);
+                i++;
+            }
+            File.WriteAllText("subjects.txt", tbListSubjects.Text);
+            lblStatus.Text = "Lấy danh sách môn học xong..";
+        }
+
+        private void BuildRequestBody(StringBuilder reqBody)
+        {
+            reqBody.Append("hidSortBy=1&hidCseChoice=");
+            reqBody.Append(tbCSE_ID.Text);
+            reqBody.Append("&hidCrdChoice=" + tbCRD_ID.Text);
+            reqBody.Append("&hidTimeChoice=" + tbLichhoc.Text);
+            reqBody.Append("&hidLengthArrUnder="+ (tbListSubjects.Text.Split(',').Length-1) + "&hidListRegOld=&hidListReg=");
+            reqBody.Append(tbhidListReg.Text.Remove(tbhidListReg.Text.Length-1));
+            reqBody.Append("&hidSubNum=" + tbhidSubNum.Text);
+
+            for (int i = 0; i < tbListSubjects.Text.Split(',').Length; i++)
+            {
+                reqBody.Append("&hidCRD_ID=&hidCSE_ID" + i + "=" + tbhidListReg.Text.Split(',')[i] + "&hidNumCse=&hidBrc1" + i + "=0&hidValueCheckBrc1" + i + "=1&hidBrc2" + i + "=0&hidValueCheckBrc2" + i + "=0&hidTypeReg" + i+"=0");
+            }
+
+            reqBody.Append("&txtSubNum=&txtNumCrd=");
+            reqBody.Replace(",", "%2C");
+            reqBody.Replace(":", "%3A");
+        }
+    }
+}
